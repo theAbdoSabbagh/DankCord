9ceffad78ba803bfd7fd327b22dbf59e4c262696
Sxvxgee <aboud1xy@gmail.com>
2022-12-03 00:46:49 +0400

    many new objects!

--- src/DankCord/Objects.py
+++ src/DankCord/Objects.py
@@ -12,12 +12,58 @@ class Response:
   
 class Message:
   def __init__(self, data: dict) -> None:
-    self.content = data["content"]
-    self.embeds = data["embeds"]
-    self.author = data["author"]
-    self.channel = data["channel_id"]
-    self.timestamp = data["timestamp"]
+    self.content : str = data["d"]["content"]
+    self.embeds : list = data["d"]["embeds"]
+    self.author : Author = Author(data["author"])
+    self.channel : int = int(data["d"]["channel_id"])
+    self.timestamp : int = data["timestamp"]
     try:
-      self.components = data["components"]
+      self.components : list = data["components"]
+      # TODO: Convert each item in list to either DropDown or Button based on the type.
     except:
-      self.components = []
+      self.components : list = []
+
+class Author:
+  def __init__(self, data : dict) -> None:
+    self.username : str = data["username"]
+    self.id : int = int(data["id"])
+    self.discriminator : int = int(data["discriminator"])
+    self.bot : bool = data["bot"]
+
+class DropdownOption:
+  def __init__(self, data : dict) -> None:
+    self.label : str = data["label"]
+    self.default : bool = data["default"]
+
+class DropdownComponent:
+  def __init__(self, data : dict) -> None:
+    self.children = [DropdownOption(child) for child in data["options"]]
+  
+  # TODO: Make a choose function
+
+class Button:
+  def __init__(self, data : dict) -> None:
+    self.emoji : Emoji = Emoji(data["emoji"])
+    self.disabled : bool = data["disabled"]
+    self.custom_id : str = data["custom_id"]
+
+  # TODO: Make a click function
+
+class Emoji:
+  def __init__(self, data : dict) -> None:
+    self.name : str = data["name"]
+    self.id : int = data["id"]
+
+class EmbedFooter:
+  def __init__(self, data : dict) -> None:
+    self.text : str = data["text"]
+    self.icon_url : str = data["icon_url"]
+    self.proxy_icon_url : str = data["proxy_icon_url"]
+
+class Embed:
+  def __init__(self, data : dict) -> None:
+    # There's 2 types: rich and image, rich is the actual embed, image is image
+    self.title : str = data["title"]
+    self.description : str = data["description"]
+    self.url : str = data["url"]
+    self.footer : EmbedFooter = EmbedFooter(data["footer"])
