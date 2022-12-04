1e6a1bd778a05e53649c211bd15833be5377705c
iylun2 <minecraftgamer00100@gmail.com>
2022-12-04 14:04:01 +0800

    pr

--- src/DankCord/logger.py
+++ src/DankCord/logger.py
@@ -1,21 +1,38 @@
 from rich import print
+
 from .Objects import Bot
 
+
 class Logger:
-  def __init__(self, bot : Bot = None) -> None:
-    self.bot = Bot(bot) if bot is not None else None
-    self.header = "[bold green]Dank[/][bold blue]Cord[/]"
-    self.barrier = "[bold black]|[/]"
-    self._bot_info = f"{self.barrier} [bold]{self.bot.bot}[/] {self.barrier}" if self.bot is not None else self.barrier
+    def __init__(self) -> None:
+        self.pipe = "[bold black]|[/]"
+        self.header = "[bold green]Dank[/][bold blue]Cord[/]"
 
-  def ws(self, /, text : str):
-    print(f"{self.header} {self._bot_info} [bold grey37]Websocket:[/] {text}")
+        self.bot = ""
+        self._bot_info = ""
 
-  def bootup(self, /, text : str, booted : bool = False):
-    print(f"{self.header} {self._bot_info} [bold grey37]{'Booted' if booted else 'Booting'} up:[/] {text}")
+    def set_dets(self, bot: dict[str, str]):
+        self.bot = Bot(bot)
+        self._bot_info = f"{self.pipe} [bold]{self.bot.bot}[/] {self.pipe}"
 
-  def error(self, /, text : str):
-    print(f"{self.header} {self._bot_info} [bold red]Error:[/] {text}")
+    def ws(self, text: str):
+        print(f"{self.header} {self._bot_info} [bold grey37]Websocket:[/] {text}")
 
-  def success(self, /, text : str):
-    print(f"{self.header} {self._bot_info} [bold green]Success:[/] {text}")
+    def bootup(self, text: str, booted: bool = False):
+        print(
+            f"{self.header} {self._bot_info} [bold grey37]{'Booted' if booted else 'Booting'} up:[/] {text}"
+        )
+
+    def error(self, text: str):
+        print(f"{self.header} {self._bot_info} [bold red]Error:[/] {text}")
+
+    def success(self, text: str):
+        print(f"{self.header} {self._bot_info} [bold green]Success:[/] {text}")
+
+    def ratelimit(self, retry_after: int, command_name: str = None):
+        message = (
+            f"[bold red]Ratelimit:[/] Trying again in {retry_after} seconds."
+            if command_name is None
+            else f"[bold red]Ratelimit with {command_name}:[/] Trying again in {retry_after} seconds."
+        )
+        print(f"{self.header} {self._bot_info} {message}")
