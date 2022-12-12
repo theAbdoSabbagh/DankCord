03361123212e86d27e587a6798aba940f37b66c7
PrivatePanda <party.mittal@gmail.com>
2022-12-12 20:37:42 +0530

    "oops"

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from src.DankCord import Config
 from pyloggor import pyloggor
 
 client = Client(
-    Config("ODU4MzQ3NzExNzk4NTc1MTE0.GUpvnX.CaYu1wv-BoPqnElrOMEfRV1ltCCAXSu5b178yo", 1043808045450412044),
+    Config("TOKEN", 00000000000),
     pyloggor(
         show_file=False,
         show_topic=False,
