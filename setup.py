b4fcf0431aec976714a80ea2fa21e242d89e9976
Sxvxgee <aboud1xy@gmail.com>
2022-12-25 15:22:52 +0400

    changed pyproject.toml to setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='DankCord',
+    version='0.0.2',
+    description='The first python library for Dank Memer selfbots. Incredibly fast, secure, strong, and reliable.',
+    author='Sxvxge',
+    author_email='sxvxge69@gmail.com',
+    url='https://www.github.com/Sxvxgee/DankCord',
+    license='MIT',
+    packages=['DankCord'],
+    install_requires=['rich', 'typing-extensions', 'orjson', 'requests'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    project_urls={
+        "Documentation": "https://github.com/Sxvxgee/DankCord/blob/master/README.md",
+        "Github": "https://www.github.com/Sxvxgee/DankCord",
+        "Changelog": "https://github.com/Sxvxgee/DankCord/blob/master/Changelog.md"
+    }
+)
