7cd7f0efd9cc3bfa504fc9826df247ee8b2e3bc9
Sxvxgee <aboud1xy@gmail.com>
2022-12-27 20:08:47 +0400

    better typehinting in all objects

--- src/DankCord/Objects.py
+++ src/DankCord/Objects.py
@@ -14,14 +14,14 @@ class Config:
         if not channel_id:
             raise ValueError("No channel_id provided.")
 
-        assert isinstance(token, str)
-        assert isinstance(channel_id, int)
-        assert resource_intensivity.upper() in ("DISK", "MEM")
+        assert isinstance(token, str), "Bot token must be of type str."
+        assert isinstance(channel_id, int), "Channel ID must be of type int."
+        assert resource_intensivity.upper() in ("DISK", "MEM"), "Resource intensivity option must be either DISK or MEM."
 
-        self.token = token
-        self.channel_id = channel_id
-        self.dm_mode = dm_mode
-        self.resource_intensivity = resource_intensivity.upper()
+        self.token: str = token
+        self.channel_id: int = channel_id
+        self.dm_mode: bool = dm_mode
+        self.resource_intensivity: str = resource_intensivity.upper()
 
 
 class Cache:
@@ -57,10 +57,10 @@ class Author:
     """
 
     def __init__(self, data: dict) -> None:
-        self.name: str = data.get("name", None)
-        self.discriminator: str = data.get("discriminator", None)
-        self.icon_url: str = data.get("icon_url", None)
-        self.id: int = int(data.get("id", 0))
+        self.name: Optional[str] = data.get("name", None)
+        self.discriminator: Optional[str] = data.get("discriminator", None)
+        self.icon_url: Optional[str] = data.get("icon_url", None)
+        self.id: Optional[int] = int(data.get("id", None))
     
     def __repr__(self) -> str:
         return f"{self.name}#{self.discriminator}"
@@ -73,7 +73,7 @@ class ActionRow:
 
     def __init__(self, data: dict, message_id: Union[str, int]):
         message_id = str(message_id) # type: ignore
-        self.components = [
+        self.components: list[Button | Dropdown] = [
             Button(i, message_id) if i["type"] == 2 else Dropdown(i, message_id) for i in data["components"]
         ]
     
@@ -84,9 +84,9 @@ class DropdownOption:
     """
 
     def __init__(self, data: dict) -> None:
-        self.label: str = data["label"]
+        self.label: Optional[str] = data.get("label", None)
         self.default: bool = data.get("default", False)
-        self.value: str = data.get("value", None)
+        self.value: Optional[str] = data.get("value", None)
 
 
 class Dropdown:
@@ -95,10 +95,10 @@ class Dropdown:
     """
 
     def __init__(self, data: dict, message_id: str) -> None:
-        self.message_id = message_id
-        self.type = 3
+        self.message_id: str = message_id
+        self.type: int = 3
         self.custom_id: Optional[int] = data.get("custom_id", None)
-        self.options = [DropdownOption(child) for child in data["options"]]
+        self.options: list[DropdownOption] = [DropdownOption(child) for child in data["options"]]
 
     # TODO: Make a choose function
 
@@ -144,12 +144,12 @@ class Embed:
     """
 
     def __init__(self, data: dict) -> None:
-        self.title: str = data.get("title", None)
-        self.description: str = data.get("description", None)
-        self.url: str = data.get("url", None)
-        self.author: Optional[Author] = Author(data["author"]) if "author" in data else None
-        self.footer: Optional[EmbedFooter] = EmbedFooter(data["footer"]) if "footer" in data else None
         self.data: dict = data
+        self.title: Optional[str] = data.get("title", None)
+        self.description: Optional[str] = data.get("description", None)
+        self.url: Optional[str] = data.get("url", None)
+        self.author: Optional[Author] = Author(data.get("author", {})) if "author" in data else None
+        self.footer: Optional[EmbedFooter] = EmbedFooter(data.get("footer", {})) if "footer" in data else None
 
 
 class Message:
@@ -160,42 +160,41 @@ class Message:
     def __init__(self, data: dict) -> None:
         self.data: dict = data
         self.author: Optional[Author] = Author(data["author"]) if "author" in data else None
-        self.content: str = data.get("content", "")
-        self.nonce: str = data.get("nonce", "")
-        self.id: int = int(data.get("id", 0))
-        self.timestamp: str = data.get("timestamp", "")
-        self.channel_id: int = int(data.get("channel_id", 0))
-        self.embeds: list = [Embed(i) for i in data.get("embeds", [])]
-        self.components: list = [ActionRow(i, self.id) for i in data.get("components", [])]
-        self.buttons: list = [
+        self.content: Optional[str] = data.get("content", None)
+        self.nonce: Optional[str] = data.get("nonce", None)
+        self.id: Optional[int] = int(data.get("id", None))
+        self.timestamp: Optional[str] = data.get("timestamp", None)
+        self.channel_id: Optional[int] = int(data.get("channel_id", None))
+        self.embeds: list[Embed] = [Embed(i) for i in data.get("embeds", [])]
+        self.components: list[ActionRow] = [ActionRow(i, self.id) for i in data.get("components", [])]
+        self.buttons: list[Button] = [
             item for component in self.components for item in component.components if isinstance(item, Button)
         ]
-        self.dropdowns: list = [
+        self.dropdowns: list[Dropdown] = [
             item for component in self.components for item in component.components if isinstance(item, Dropdown)
         ]
-
-class Bot:
-    """
-    Represents the bot account.
-    """
-    
-    def __init__(self, data: dict) -> None:
-        self.username: str = data["d"]["user"]["username"]
-        self.id: int = int(data["d"]["user"]["id"])
-        self.discriminator: int = int(data["d"]["user"]["discriminator"])
-        self.email: str = data["d"]["user"]["email"]
-        self.bot: str = f"{self.username}#{self.discriminator}"
 
 class CommandResult:
     """
     Represents a class that has the result of running a certain command.
     """
     
-    def __init__(self, success: bool, death: bool, gain: dict, loss: dict) -> None:
-        self.success: bool = success
-        self.death: bool = death
-        self.gain: dict = gain
-        self.loss: dict = loss
+    def __init__(
+        self,
+        success: bool = None,
+        death: bool = None,
+        coin_gain: int = None,
+        coin_loss: int = None,
+        items_gain: dict = None,
+        items_loss: dict = None,
+    ) -> None:
+        self.success: Optional[bool] = success
+        self.death: Optional[bool] = death
+        self.coin_gain: Optional[int] = coin_gain
+        self.coin_loss: Optional[int] = coin_loss
+        self.items_gain: Optional[dict] = items_gain
+        self.items_loss: Optional[dict] = items_loss
+
 
 class User:
     """
@@ -203,9 +202,9 @@ class User:
     """
 
     def __init__(self, data: dict) -> None:
-        self.id: int = int(data.get("id", 0))
-        self.discriminator: int = int(data.get("discriminator", 0))
-        self.name: str = data.get("username", "")
+        self.id: Optional[int] = int(data.get("id", None))
+        self.discriminator: Optional[int] = int(data.get("discriminator", None))
+        self.name: Optional[str] = data.get("username", None)
         self.bio: Optional[str] = data.get("bio", None)
         self.phone: Optional[str] = data.get("phone", None)
         self.email: Optional[str] = data.get("email", None)
