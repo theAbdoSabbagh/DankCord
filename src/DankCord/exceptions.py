e730735ac09765aab8d709c8aa1ce03c57d66026
Sxvxgee <aboud1xy@gmail.com>
2022-12-29 15:04:27 +0400

    new exception, removed unused ones

--- src/DankCord/exceptions.py
+++ src/DankCord/exceptions.py
@@ -2,14 +2,6 @@ class DankCordException(BaseException):
     pass
 
 
-class InvalidCommand(DankCordException):
-    pass
-
-
-class InvalidComponent(DankCordException):
-    pass
-
-
 class InvalidToken(DankCordException):
     pass
 
@@ -22,12 +14,13 @@ class UnknownChannel(DankCordException):
     pass
 
 
-class NonceTimeout(DankCordException):
-    pass
-
-
 class NoCommands(DankCordException):
     pass
 
+
 class DataAccessFailure(DankCordException):
+    pass
+
+
+class InvalidFormBody(DankCordException):
     pass
