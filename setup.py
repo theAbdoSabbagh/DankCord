8627fb348c3c758dbb7372a09f7561446cb15ecd
Sxvxgee <aboud1xy@gmail.com>
2022-12-29 15:07:22 +0400

    updated version, 2 new project urls, removed 1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='DankCord',
-    version='0.0.2.3.5',
+    version='0.0.2.4',
     description='The first python library for Dank Memer selfbots. Incredibly fast, secure, strong, and reliable.',
     author='Sxvxge',
     author_email='sxvxge69@gmail.com',
@@ -13,8 +13,9 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     project_urls={
+        "Author Portfolio": "https://sxvxge.dev",
         "Documentation": "https://github.com/Sxvxgee/DankCord/blob/master/README.md",
         "Github": "https://www.github.com/Sxvxgee/DankCord",
-        "Changelog": "https://github.com/Sxvxgee/DankCord/blob/master/Changelog.md"
+        "Discord": "https://discord.gg/XaQ6FAP3sm"
     }
 )
